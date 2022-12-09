7cd24c1724f1f700d2e88127b3776f9c59097170
walid taib <walidtaib44@gmail.com>
2022-12-09 01:12:55 -0800

    react redux form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -69,7 +69,7 @@ class Main extends Component{
               <div>
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />}  />
                     <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
